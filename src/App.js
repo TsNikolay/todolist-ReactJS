13db978d5550ce794b169f89b2aa0a68ae87af6e
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-04-09 14:35:28 +0200

    Added ability to add tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
+import AddTodo from "./Todo/addTodo";
 
 function App() {
     const [todoList, setTodos] = React.useState([
@@ -9,9 +10,10 @@ function App() {
         {id: 3, completed: false, title: "Вытереть пыль"},
         {id: 4, completed: false, title: "Продать гараж"},
     ])
-    function switchTodo(id){
-        setTodos(todoList.map( todo => {
-                if(todo.id === id){
+
+    function switchTodo(id) {
+        setTodos(todoList.map(todo => {
+                if (todo.id === id) {
                     todo.completed = !todo.completed;
                 }
                 return todo
@@ -19,15 +21,25 @@ function App() {
         )
     }
 
-    function removeTodo(id){
+    function removeTodo(id) {
         setTodos(todoList.filter(todo => todo.id !== id))
     }
+
+    function addTodo(title) {
+        setTodos(todoList.concat([{
+            title,
+            id: Date.now(),
+            completed: false
+        }]))
+    }
+
     return (
         <Context.Provider value={{removeTodo}}>
-        <div className="wrapper">
-            <h1>My TodoList</h1>
-            <TodoList tasks={todoList} onSwitch={switchTodo}/>
-        </div>
+            <div className="wrapper">
+                <h1>My TodoList</h1>
+                <AddTodo onCreate={addTodo}/>
+                {todoList.length ? <TodoList tasks={todoList} onSwitch={switchTodo}/> : <p>You do not have tasks</p>}
+            </div>
         </Context.Provider>);
 }
 
